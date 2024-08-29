706a0933a87c4cfcaf0767cacc3f1f795324154a
vivek <vivekkajale302@gmail.com>
2024-08-29 19:54:40 +0530

    Object_end and added score to rock paper scissors use built in object (json, local-storage) learn shorcuts like(destructuring, shorthand property, shorthand method)

---
+++ 08Objects.js
@@ -0,0 +1,10 @@
+const product = {
+    name: 'socks',
+    Price: 899
+};
+console.log(product);
+console.log(product.name);
+console.log(product.Price);
+// changing property of name
+product.name = 'Cotton socks';
+console.log(product);
